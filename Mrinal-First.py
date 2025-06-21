4010b8bbfd305cda09766a8e193c9010ae17e8d8
mrinalbd <mrinalbd@gmail.com>
2025-06-21 20:14:55 +0600

    Hello

---
+++ Mrinal-First.py
@@ -0,0 +1,30 @@
+# print('Hello Bangladesh, I love you very mutch.')
+# a = 20
+# b = 30
+# print(a)
+# #if a < b:
+# print (a+b)
+# c=2*a+b
+# print(c)
+# X=15
+# Y=10
+# if (X<Y):
+# print ('X is smaller than Y')
+# else:
+#  print('Y is bigger')
+# Z=X+Y
+# print(Z)
+# ' ' " " ''' ''' """ """
+# a = '10'
+# print (type(a))
+a = '''Today is my birthday-------'''
+# print(a[5:])
+# print(a.upper())
+# print(a.lower())
+# print(a.capitalize())
+# print (len(a))
+a = input('Insert your City:')
+c = 2025
+b = f'My City is {a}. I love my City very much. This is {c}.'
+# print (b.format(a,c))
+print (b)
